5fc4dce313860a463ef346dca78bdf7faf655b69
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2024-10-29 00:14:05 +0530

    codes

---
+++ PigeonHole_Principal/Subarray_Sum_Divisible_by_N.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int> nums){
+    vector<int>prefix(n+1, 0);
+}
+
+int main(){
+    int n = 5;
+    vector<int> nums = {4, 5, 0, 4, 2};
+
+    cout << solve(n, nums) << endl;
+}
